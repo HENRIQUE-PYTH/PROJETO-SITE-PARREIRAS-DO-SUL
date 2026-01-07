7f053a7c314324f13dbc5bdaf50155625e22cfa6
Thiago.GT.NEV <caselasthiago@gmail.com>
2026-01-07 09:53:37 -0300

    arrumar branco header e responsivo footer, etc.             [NEV]²Thi_ii

---
+++ PROJETO-SITE-PARREIRAS-DO-SUL-main/js/contato.js
@@ -0,0 +1,43 @@
+// =======================
+// Máscara para telefone
+// =======================
+const telefoneInput = document.getElementById("telefone");
+
+telefoneInput.addEventListener("input", function (e) {
+    let x = e.target.value.replace(/\D/g, "");
+    if (x.length > 11) x = x.slice(0, 11);
+    x = x.replace(/^(\d{2})(\d)/, "($1) $2");
+    x = x.replace(/(\d{5})(\d)/, "$1-$2");
+    e.target.value = x;
+});
+
+// =======================
+// Formulário via WhatsApp
+// =======================
+const whatsappForm = document.getElementById("whatsappForm");
+
+whatsappForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const nome = document.getElementById("nome").value;
+    const telefone = document.getElementById("telefone").value;
+    const mensagem = document.getElementById("mensagem").value;
+
+    const numeroVincula = "5561996604875";
+
+    const texto = 
+`Olá! Tudo bem?
+
+Meu nome é ${nome}.
+Telefone para contato: ${telefone}.
+
+Mensagem:
+"${mensagem}"`;
+
+    const url = `https://wa.me/${numeroVincula}?text=${encodeURIComponent(texto)}`;
+    window.open(url, "_blank");
+
+    whatsappForm.reset();
+});
+
+
